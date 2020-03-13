c140fb90b6febe36e6ef170fee4b0be32d15857e
lehuyduc <lehuyduc3@gmail.com>
2020-03-13 22:40:15 +0700

    Updated labwork 3 - shell so ls can now print

--- labwork3.c
+++ labwork3.c
@@ -7,13 +7,17 @@ void parseArgs(char *s, char *args[]) {
     char arg[100];
 
     n = strlen(s);
+    while (s[n-1]==' ' || s[n-1]=='\n') n--;
+    s[n] = NULL;
+
     nargs = 0;
     for (i=0; i<n;i++) {
         j = i;
         while (j<n && s[j]!=' ') j++;
-        if (j==n) j--;
+        j--;
         memcpy(arg, &s[i], j-i+1);
-        i = j;
+        arg[j-i+1] = 0;
+        i = j+1;
 
         nargs++;
         args[nargs-1] = malloc(sizeof(char)*100);
@@ -28,9 +32,11 @@ int main()
     char *s[100];
 
     while (true) {
-        scanf("%s",s);
-        printf("user typed: %s\n",s);
+        //scanf("%s",s);
+        fgets(s, sizeof(s), stdin);
+        printf("user typed: %s",s);
         fflush(stdout);
+
         if (!strcmp(s, "quit")) {
             printf("User quit\n");
             return 0;
@@ -40,6 +46,10 @@ int main()
 
         int pid = fork();
         if (pid==0) execvp(args[0], args);
+        else {
+            wait();
+        }
+
     }
 
 	return 0;
